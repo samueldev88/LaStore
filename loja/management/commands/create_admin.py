33ebc371439ed63e7e1d2fe347f55429fbe24d37
Samuel <samuelcordeirodearaujo@gmail.com>
2026-09-13 17:49:49 -0300

    adiciona comando create_admin para criar superusuario no deploy

---
+++ loja/management/commands/create_admin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+        if username and password and not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write('Superusuário criado com sucesso')
+        else:
+            self.stdout.write('Superusuário já existe ou variáveis não configuradas')
